03d3ee6259f233e41949e816248ee3f8809cc976
David Dinkins <d.b.dinkins@gmail.com>
2022-10-05 06:23:43 -0500

    adds signature encoder module for producing signatures sent in requests to gateway's api

---
+++ lib/payment_gateway/signature_encoder.ex
@@ -0,0 +1,38 @@
+defmodule PaymentGateway.SignatureEncoder do
+
+  # to be stored as environment/config variables
+  @merchant_api_key "4Vj8eK4rloUd272L48hsrarnUA"
+  @merchant_account_id "512321"
+
+  def reference_code(cart) do
+    "#{first_sku(cart)}_#{env()}_#{Date.utc_today}"
+  end
+
+  defp first_sku(cart) do
+    cart.skus
+    |> Map.keys
+    |> List.first
+  end
+
+  defp env() do
+    Mix.env()
+    |> Atom.to_string
+    |> String.upcase
+  end
+
+  def order_description() do
+    "Payment test description"
+  end
+
+  def order_signature(cart) do
+    string = "#{@merchant_api_key}~#{@merchant_account_id}" <>
+             "~#{reference_code(cart)}~#{cart.total_transaction_price}" <>
+             "~#{cart.currency}"
+
+    :crypto.hash(:md5, string) |> Base.encode16(case: :lower)
+  end
+
+  def device_session_id_signature(cookie) do
+    :crypto.hash(:md5, cookie) |> Base.encode16(case: :lower)
+  end
+end
---
+++ test/payment_gateway/signature_encoder_test.exs
@@ -0,0 +1,36 @@
+defmodule PaymentGateway.SignatureEncoderTest do
+  use PaymentGateway.CartCase
+
+  # to be stored as environment/config variables
+  @merchant_api_key "4Vj8eK4rloUd272L48hsrarnUA"
+  @merchant_account_id "512321"
+
+  test "reference_code/1 returns string with cart's first sku, env & date stamp", %{ cart: cart} do
+    ref_code_list =
+      cart
+      |> reference_code
+      |> String.split("_")
+
+    env =
+      ref_code_list
+      |> Enum.at(1)
+      |> String.downcase
+      |> String.to_atom
+
+    assert Map.has_key?(cart.skus, Enum.at(ref_code_list, 0))
+    assert env == Mix.env()
+    assert Enum.at(ref_code_list, 2) == Date.to_string(Date.utc_today)
+  end
+
+  test "order_signature/1 returns md5 hash", %{ cart: cart } do
+    assert String.length(order_signature(cart)) == String.length(md5_hash("foo"))
+  end
+
+  test "device_session_id_signature/1 returns md5 hash", %{ cart: cart } do
+    assert String.length(device_session_id_signature(cart.cookie)) == String.length(md5_hash("bar"))
+  end
+
+  defp md5_hash(string) do
+    :crypto.hash(:md5, string) |> Base.encode16(case: :lower)
+  end
+end
