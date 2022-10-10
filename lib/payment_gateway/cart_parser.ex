a0cf3520d3c7bec969c746706fa4d3253acb8af3
David Dinkins <d.b.dinkins@gmail.com>
2022-10-10 06:54:50 -0500

    moves parse/1's pipeline into order request builder

--- lib/payment_gateway/cart_parser.ex
+++ lib/payment_gateway/cart_parser.ex
@@ -1,17 +1,21 @@
 defmodule PaymentGateway.CartParser do
 
+  @payu_latam_countries ["CO", "BR", "PA", "CL", "PE", "AR", "MX"]
   import PaymentGateway.OrderRequestBuilder
 
-  def parse(cart) do
-    cart
-    |> add_merchant_info
-    |> add_transaction_order
-    |> add_transaction_buyer
-    |> add_transaction_shipping_address
-    |> add_payer
-    |> add_credit_card
-    |> add_extra_parameters
-    |> add_test
-    |> encode
+  def parse({:payu_latam, _cart} = order_data) do
+    # TODO: handle incomplete cart error ahead of pipeline
+    build_request_data(order_data)
+  end
+  def parse({:error, _reason} = error), do: error
+  def parse({_gateway, _cart}), do: {:error, "unrecognized gateway"}
+
+  def select_gateway(%{ payment_country: country } = cart) do
+    case Enum.member?(@payu_latam_countries, country) do
+      true ->
+        {:payu_latam, cart}
+      _ ->
+        {:error, "no support for non Payu Latam countries atm"}
+    end
   end
 end
--- test/payment_gateway/cart_parser_test.exs
+++ test/payment_gateway/cart_parser_test.exs
@@ -1,11 +1,32 @@
 defmodule PaymentGateway.CartParserTest do
-  use PaymentGateway.CartCase
+  use PaymentGateway.CartCase, async: true
   alias PaymentGateway.CartParser
   doctest CartParser
 
-  test "parse/1 converts cart's data into json string for order transaction", %{ cart: cart } do
-    assert json = CartParser.parse(cart)
-    assert is_binary(json)
-    assert {:ok, _map} = Jason.decode(json)
+  describe "parse/1" do
+    test "returns all data required for order transaction request", %{ cart: cart } do
+      assert {:ok, :payu_latam, _url, _request_body} = CartParser.parse({:payu_latam, cart})
+    end
+
+    test "returns error for unrecognized gateways" do
+      assert CartParser.parse({:epay, %{}}) == {:error, "unrecognized gateway"}
+    end
+
+    test "returns error tuple when receiving {:error, _reason} tuple" do
+      assert {:error, reason} = CartParser.parse({:error, "generic error message"})
+      assert reason == "generic error message"
+    end
+  end
+
+  describe "select_gateway/1" do
+    test "returns {:payu_latam, _} tuple
+          when cart.payment_country is one of [CO, BR, PA, CL, AR, PE, MX]", %{ cart: cart } do
+      assert {:payu_latam, _cart} = CartParser.select_gateway(cart)
+      assert {:payu_latam, _cart} = CartParser.select_gateway(%{ payment_country: "BR" })
+    end
+
+    test "returns {:error, _} tuple when cart.payment country is not in the payu latam list" do
+      assert {:error, _} = CartParser.select_gateway(%{ payment_country: "US" })
+    end
   end
 end
