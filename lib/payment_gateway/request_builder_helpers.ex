be5013aa4cd3b99395ab79b1a5214decd1dfcffc
David Dinkins <d.b.dinkins@gmail.com>
2022-10-15 10:59:08 -0500

    updates request_builder_helpers module with payu_latam_test_account_id/1

--- lib/payment_gateway/request_builder_helpers.ex
+++ lib/payment_gateway/request_builder_helpers.ex
@@ -1,4 +1,16 @@
 defmodule PaymentGateway.RequestBuilderHelpers do
+
+  @payu_latam_country_codes ["CO", "BR", "PA", "CL", "PE", "AR", "MX"]
+  @payu_latam_test_account_ids %{
+    "AR" => "512322",
+    "BR" => "512327",
+    "CL" => "512325",
+    "CO" => "512321",
+    "MX" => "512324",
+    "PA" => "512326",
+    "PE" => "512323"
+  }
+
   def ip_address() do
     {:ok, ifs} = :inet.getif()
     ips = Enum.map(ifs, fn {ip, _broadaddr, _mask} -> ip end)
@@ -7,4 +19,9 @@ defmodule PaymentGateway.RequestBuilderHelpers do
 
     "#{n1}.#{n2}.#{h1}.#{h2}"
   end
+
+  def payu_latam_test_account_id(code) when code in @payu_latam_country_codes do
+    Map.get(@payu_latam_test_account_ids, code)
+  end
+  def payu_latam_test_account_id(_code), do: nil
 end
