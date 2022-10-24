e7b8cf2a2b441c8ba7cf964bf2aa80c67c15eb23
David Dinkins <d.b.dinkins@gmail.com>
2022-10-24 07:20:50 -0500

    adds api call functions to payment gateway module

--- lib/payment_gateway.ex
+++ lib/payment_gateway.ex
@@ -2,7 +2,27 @@ defmodule PaymentGateway do
   import PaymentGateway.{CartParser, Gateway}
 
   def checkout(cart) do
-    cart
+    {:checkout, cart} |> call_gateway_api
+  end
+
+  def tokenize_credit_card(cart) do
+    {:tokenize_credit_card, cart} |> call_gateway_api
+  end
+
+  def delete_token(cart) do
+    {:delete_token, cart} |> call_gateway_api
+  end
+
+  def query_tokens(cart) do
+    {:query_tokens, cart} |> call_gateway_api
+  end
+
+  def pay_with_token(cart) do
+    {:pay_with_token, cart} |> call_gateway_api
+  end
+
+  defp call_gateway_api({_request_type, _cart} = request_data) do
+    request_data
     |> select_gateway
     |> parse
     |> send_api_call
--- test/payment_gateway_test.exs
+++ test/payment_gateway_test.exs
@@ -2,11 +2,77 @@ defmodule PaymentGatewayTest do
   use PaymentGateway.CartCase, async: true
   doctest PaymentGateway
 
+  @tag :api_call
   describe "checkout/1" do
-    test "when transaction's api call is successfull returns a {:ok, response_body} tuple",
+    test "when transaction's api call is successful returns a {:ok, response_body} tuple",
       %{ cart: cart } do
-      assert {:ok, response_body} = PaymentGateway.checkout(cart)
+      case PaymentGateway.checkout(cart) do
+        {:ok, response_body} ->
+          assert is_map(response_body)
+        {:error, message} ->
+          assert message =~ "timed out"
+      end
+    end
+  end
+
+  @tag :api_call
+  describe "tokenize_credit_card/1" do
+    test "when transaction's api call is successful returns a {:ok, response_body} tuple",
+      %{ cart: cart } do
+      assert {:ok, response_body} = PaymentGateway.tokenize_credit_card(cart)
       assert is_map(response_body)
     end
+  end
+
+  @tag :api_call
+  describe "delete_token/1" do
+    test "when transaction's api call is successful returns a {:ok, response_body} tuple",
+      %{ cart: cart } do
+      cart_with_token = create_cart_with_token(cart)
+
+      assert {:ok, response_body} = PaymentGateway.delete_token(cart_with_token)
+      assert is_map(response_body)
+    end
+  end
+
+  @tag :api_call
+  describe "query_tokens/1" do
+    test "when transaction's api call is successful returns a {:ok, response_body} tuple",
+      %{ cart: cart } do
+      cart_with_token = create_cart_with_token(cart)
+
+      assert {:ok, response_body} = PaymentGateway.query_tokens(cart_with_token)
+      assert is_map(response_body)
+    end
+  end
+
+  @tag :api_call
+  describe "pay_with_token/1" do
+    test "when transaction's api call is successful returns a {:ok, response_body} tuple",
+      %{ cart: cart } do
+      cart_with_token = create_cart_with_token(cart)
+
+      case PaymentGateway.pay_with_token(cart_with_token) do
+        {:ok, response_body} ->
+          assert is_map(response_body)
+        {:error, message} ->
+          assert message =~ "timed out"
+      end
+    end
+  end
+
+  defp create_cart_with_token(cart) do
+    {:ok, response_body} = PaymentGateway.register_credit_card(cart)
+    token_id = response_body["creditCardToken"]["creditCardTokenId"]
+
+    credit_card =
+      cart
+      |> Map.fetch!(:credit_card)
+      |> Map.put(:token_id, token_id)
+
+    cart_with_token =
+      Map.put(cart, :credit_card, credit_card)
+
+    cart_with_token
   end
 end
