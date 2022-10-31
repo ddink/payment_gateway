644ef19259dab2f3a380a727f2e9d6f895dd41cf
David Dinkins <d.b.dinkins@gmail.com>
2022-10-30 22:26:14 -0500

    implements payment gateway as a genserver

--- lib/payment_gateway.ex
+++ lib/payment_gateway.ex
@@ -1,31 +1,46 @@
 defmodule PaymentGateway do
   import PaymentGateway.{CartParser, Gateway}
+  use GenServer
+
+  @impl true
+  def init(state \\ %{}) do
+    {:ok, state}
+  end
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
 
   def checkout(cart) do
-    {:checkout, cart} |> call_gateway_api
+    GenServer.call(__MODULE__, {:checkout, cart}, 6_000)
   end
 
   def tokenize_credit_card(cart) do
-    {:tokenize_credit_card, cart} |> call_gateway_api
+    GenServer.call(__MODULE__, {:tokenize_credit_card, cart}, 6_000)
   end
 
   def delete_token(cart) do
-    {:delete_token, cart} |> call_gateway_api
+    GenServer.call(__MODULE__, {:delete_token, cart}, 6_000)
   end
 
   def query_tokens(cart) do
-    {:query_tokens, cart} |> call_gateway_api
+    GenServer.call(__MODULE__, {:query_tokens, cart}, 6_000)
   end
 
   def pay_with_token(cart) do
-    {:pay_with_token, cart} |> call_gateway_api
+    GenServer.call(__MODULE__, {:pay_with_token, cart}, 6_000)
   end
 
-  defp call_gateway_api({_request_type, _cart} = request_data) do
+  defp call_gateway_api({request_type, _cart} = request_data) when is_atom(request_type) do
     request_data
     |> select_gateway
     |> parse
-    |> send_api_call
-    |> handle_transaction_response
+    |> send_request
+  end
+
+  @impl true
+  def handle_call({_gateway, _cart} = request_data, _from, state) do
+    response = request_data |> call_gateway_api
+    {:reply, response, state}
   end
 end
--- test/payment_gateway_test.exs
+++ test/payment_gateway_test.exs
@@ -6,12 +6,8 @@ defmodule PaymentGatewayTest do
   describe "checkout/1" do
     test "when transaction's api call is successful returns a {:ok, response_body} tuple",
       %{ cart: cart } do
-      case PaymentGateway.checkout(cart) do
-        {:ok, response_body} ->
-          assert is_map(response_body)
-        {:error, message} ->
-          assert message =~ "timed out"
-      end
+
+      assert_successful_api_call(&PaymentGateway.checkout/1, cart)
     end
   end
 
@@ -52,27 +48,31 @@ defmodule PaymentGatewayTest do
       %{ cart: cart } do
       cart_with_token = create_cart_with_token(cart)
 
-      case PaymentGateway.pay_with_token(cart_with_token) do
-        {:ok, response_body} ->
-          assert is_map(response_body)
-        {:error, message} ->
-          assert message =~ "timed out"
-      end
+      assert_successful_api_call(&PaymentGateway.pay_with_token/1, cart_with_token)
     end
   end
 
   defp create_cart_with_token(cart) do
-    {:ok, response_body} = PaymentGateway.register_credit_card(cart)
+    {:ok, response_body} = PaymentGateway.tokenize_credit_card(cart)
     token_id = response_body["creditCardToken"]["creditCardTokenId"]
 
-    credit_card =
+    payment_method =
       cart
-      |> Map.fetch!(:credit_card)
-      |> Map.put(:token_id, token_id)
+      |> Map.fetch!(:payment_method)
+      |> Map.put(:cc_token_id, token_id)
 
     cart_with_token =
-      Map.put(cart, :credit_card, credit_card)
+      Map.put(cart, :payment_method, payment_method)
 
     cart_with_token
+  end
+
+  defp assert_successful_api_call(fun, args) when is_function(fun) do
+    case fun.(args) do
+      {:ok, response_body} ->
+        assert is_map(response_body)
+      {:error, message} ->
+        assert message =~ "timed out"
+    end
   end
 end
